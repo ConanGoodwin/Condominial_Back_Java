c57ed1e55ba28f1fe83376e76bb0503f21dad483
ConanGoodwin <conan_goodwin@hotmail.com>
2024-11-03 15:23:34 -0300

    chore: projeto iniciado :rocket: Repositorio GitHub criado Instalado pacotes Porijeto spring javen java subido para repositorio

---
+++ src/main/java/com/conangoodwin/condominial/CondominialApplication.java
@@ -0,0 +1,13 @@
+package com.conangoodwin.condominial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondominialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondominialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conangoodwin/condominial/CondominialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conangoodwin.condominial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondominialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
